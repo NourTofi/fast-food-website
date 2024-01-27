ef6e68e380aa18fbc5d1a7fe5c34d275ec0c5cd2
Nour Tofi <Nour.Tofi.IT@gmail.com>
2024-01-27 13:38:50 +0300

    add eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2016: true,
   },
   extends: [
     "eslint:recommended",
@@ -26,9 +26,6 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {
-    "prettier/prettier": "error",
-  },
   settings: {
     react: {
       version: "detect",
